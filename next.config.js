d11a8269be0127bcd12a3e0cff93c9cc13f43bd9
aryanmdh <mallikarjuncrazysoul@gmail.com>
2023-05-03 18:02:01 +0530

    feat(next-config): add image hosts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.dummyjson.com',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
